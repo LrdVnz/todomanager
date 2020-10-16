1133de6afec49dba0234fb6c8362577255fda55d
lrdvnz <vinzgameslol@gmail.com>
2020-10-16 12:46:48 +0200

    delete button working

--- views/index.view.php
+++ views/index.view.php
@@ -1,53 +1,50 @@
 <?php require('partials/head.php') ?>
 
-   <h3>task manager</h3>
+<h3>task manager</h3>
 
-  <ul>
-    <?php foreach($tasks as $task) : ?>
+<ul>
+  <?php foreach ($tasks as $task) : ?>
     <li>
 
-     <?php if($task->completed) : ?>
-     
-      <s><?= $task->descr; ?></s>
-     
-      <?php else :?> 
-     
-        <?= $task->descr; ?> 
-     
-        <?php endif; ?>
-     
-      </li>
-    
-      <?php endforeach; ?>
-  </ul>
+      <?php if ($task->completed) : ?>
+
+        <s><?= $task->descr; ?></s>
+
+      <?php else : ?>
+
+        <?= $task->descr; ?>
+
+      <?php endif; ?>
+
+      <div id="delete">
+      <form method="POST" action="deleteTasks">
+        <button type="submit" name="delete" value= <?= $task->descr;?> >delete</button>
+      </form>
+    </div>
+
+    </li>
+
+  <?php endforeach; ?>
+</ul>
 
 
-  <div id = "create">
-    <h4>Create a new task</h4>
-
-    <form method="POST" action="tasks">
-    
+<div id="create">
+  <h4>Create a new task</h4>
+  <form method="POST" action="tasks">
     <input type="text" name="task"></input>
     <button type="submit">Submit</button>
-    </form>
-  </div>
+  </form>
+</div>
 
- <div id="delete">
-   <h4>Delete a task. Put the task name</h4>
-   <form method="POST" action="deleteTasks">
-    <input type="text" name="delete" ></input>
-    <button type="submit">Submit</button>
-   </form>
- </div>
 
- <div id="modify">
-   <h4>Modify. Put the task name</h4>
-   <form method="POST" action="modifyTasks">
+<div id="modify">
+  <h4>Modify. Put the task name</h4>
+  <form method="POST" action="modifyTasks">
     <input type="text" name="oldDescr"></input>
     <p>Put the updated name</p>
     <input type="text" name="newDescr"></input>
     <button type="submit">Submit</button>
-   </form>
- </div>
+  </form>
+</div>
 
 <?php require('partials/footer.php') ?>
